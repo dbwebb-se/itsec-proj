33aa920829b24d1faa880cf1d72e7b3a432ca9c0
Kenneth Lewenhagen <lewenhagen@live.com>
2019-10-09 14:59:31 +0200

    adlfasf

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const port = 1337;
-let bodyParser = require("body-parser");
-let cookieParser = require('cookie-parser');
+// let bodyParser = require("body-parser");
+// let cookieParser = require('cookie-parser');
 let dbmodule = require('./src/database');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
 
 app.use(session({
     key: "user_id",
@@ -20,59 +20,89 @@ app.use(session({
     cookie: {
         expires: 600000
     }
-}));
 
-// app.use((req, res, next) => {
-//     if (req.cookies.user_id && !req.session.user) {
-//         res.clearCookie('user_id');
-//     }
-//     next();
-// });
+}));
 
 app.use((req, res, next) => {
     res.locals.session = req.session;
     next();
 });
 
-dbmodule.connect();
+// function isAdmin(req, res, next) {
+//     console.log(req.session.user.name);
+//     if (req.session.user.name === "admin") {
+//       return next();
+//     }
+//
+//     res.redirect('/manage-user');
+// }
+
+// dbmodule.connect();
 
 app.get('/', (req, res) => res.render('pages/index'));
+
 app.get('/manage', async (req, res) => {
-    let all;
-    if (req.session.user === "admin") {
-        all = await dbmodule.selectAll();
+    let user = req.session.user;
+
+    if (user === undefined) {
+        req.session.flash = "You have to login first.";
+        res.redirect(302, '/login');
+    } else if (user.name === "admin") {
+        res.render('pages/admin-manage.ejs');
     } else {
-        all = await dbmodule.selectOne(req.session.user);
+
+
+        res.render('pages/user-manage.ejs');
     }
-    app.locals.all = all;
-    res.render('pages/manage');
+    // let username;
+    //
+    // if (user === "admin" && req.query.manage_user) {
+    //     app.locals.usernames = await dbmodule.selectAllUsers();
+    //     username = await dbmodule.getUsernameById(req.query.manage_user);
+    //     app.locals.oneUser = await dbmodule.selectOneUser(username[0].name);
+    //     app.locals.accounts = await dbmodule.getAccount(username[0].name);
+    //     req.session.manage_user = req.query.manage_user;
+    //
+    // } else if (user === "admin") {
+    //     app.locals.usernames = await dbmodule.selectAllUsers();
+    // } else {
+    //     app.locals.oneUser = await dbmodule.selectOneUser(user);
+    //     app.locals.accounts = await dbmodule.getAccount(user);
+    // }
+    // res.render('pages/manage.ejs');
+});
+
+
+app.get('/manage-user', async (req, res) => {
+    app.local.data = ""
+
+    res.render('pages/manage.ejs');
 });
 
 app.get('/login', (req, res) => {
     if (req.session.user) {
-        req.session.flash = "Already logged in as: " + req.session.user;
+        req.session.flash = "Already logged in as: " + req.session.user.name;
     }
     res.render('pages/login.ejs');
 });
 
 app.get('/logout', (req, res) => {
     if (req.session.user) {
-        req.session.flash = "You have successfully logged out."
         req.session.user = null;
+        req.session.flash = "You have successfully logged out."
     }
     res.redirect(302, '/login');
 });
 
 app.get('/login-error', (req, res) => {
     req.session.flash = "Something went wrong. Try again.";
-
     res.redirect(302, '/login');
 });
 
-app.get('/login-success', (req, res) => {
-    let user = req.query.user;
-    req.session.user = user;
-    req.session.flash = "Welcome " + user;
+app.get('/login-success', async (req, res) => {
+    let user = await dbmodule.selectOneUser(req.query.user);
+    req.session.user = user[0];
+    req.session.flash = "Welcome " + req.session.user.name;
     res.render("pages/login.ejs");
 });
 
@@ -80,11 +110,46 @@ app.get('/signup', (req, res) => res.render('pages/signup'));
 
 app.get('/processsignup', async (req, res) => {
     let userId = await dbmodule.createUser(req.query);
-    console.log("Id of new user: " + userId);
-    let moneyId = await dbmodule.connectUserToMoney(userId);
-    console.log("Id of newly connected account" + moneyId);
+    // let moneyId = await dbmodule.connectUserToMoney(userId);
+
     req.session.flash = req.query.username + " is created. Please login.";
     res.redirect(302, '/login');
+});
+
+app.get('/create-account', async (req, res) => {
+    let user_id = req.query.new_account_user_id;
+    let accountName = req.query.new_accountname;
+    let result = await dbmodule.createAccount(user_id, accountName);
+
+    res.redirect(302, '/manage?manage_user=' + user_id);
+});
+
+
+
+app.get('/update', async (req, res) => {
+    let form = req.query.change;
+    let rowsChanges;
+    req.session.flash = "Updated ";
+    if (form === "Change user") {
+        rowsChanged = await dbmodule.updateUser(req.query);
+        console.log("asdf");
+        req.session.flash += "user: " + req.query.name;
+    } else if (form === "Change account") {
+        let accIds = req.query.acc_id;
+        for (let i = 0; i < accIds.length; i++) {
+            rowsChanged = await dbmodule.updateAccount(req.query.acc_name[i], req.query.acc_amount[i], req.query.acc_id[i]);
+        }
+        req.session.flash += "an account";
+    }
+    res.redirect('/manage');
+});
+
+app.get('/delete-account', async (req, res) => {
+    let accId = req.query.del_account;
+    req.session.flash = "Deleted account with id: " + accId;
+    result = await dbmodule.deleteAccount(accId);
+
+    res.redirect(302, '/manage');
 });
 
 
--- src/database.js
+++ src/database.js
@@ -1,22 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
 
 let theDb = {
-    db: {},
-    connect: function() {
-        this.db = new sqlite3.Database('./db/bank.db', sqlite3.OPEN_READWRITE, (err) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                console.log('Connected to the bank database.');
-            }
-        });
-    },
-    close: function() {
-        this.db.close();
-    },
+    db: new sqlite3.Database('./db/bank.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Connected to the bank database.');
+        }
+    }),
     selectAllUsers: function() {
-        let sql = `SELECT * FROM users`;
-        let result = [];
+        let sql = `SELECT id, name, pass FROM users`;
         return new Promise ((resolve, reject) => {
             this.db.all(sql, [], (err, rows) => {
                 if (err) {
@@ -26,15 +19,12 @@ let theDb = {
                 }
             });
         })
-
     },
     selectAll: function() {
         let sql = `
-            SELECT u.id, u.name, u.pass, m.amount
-            FROM users AS u
-                INNER JOIN money AS m
-            ON u.id = m.user_id`;
-        let result = [];
+            SELECT id, name, pass
+            FROM users`;
+
         return new Promise ((resolve, reject) => {
             this.db.all(sql, [], (err, rows) => {
                 if (err) {
@@ -45,16 +35,28 @@ let theDb = {
             });
         })
     },
-    selectOne: function(user) {
-        console.log("this is the user: " + user);
+    getUsernameById: function(userid) {
         let sql = `
-        SELECT u.id, u.name, u.pass, m.amount
-            FROM users AS u
-            INNER JOIN money AS m
-                ON m.user_id = u.id
-        WHERE u.name = ?`;
-        // console.log(sql);
+            SELECT name
+                FROM users
+            WHERE id = ?`;
         let result = [];
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [userid], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        })
+    },
+    selectOneUser: function(user) {
+        let sql = `
+        SELECT id, name, pass
+            FROM users
+        WHERE name = ?`;
+
         return new Promise ((resolve, reject) => {
             this.db.all(sql, [user], (err, rows) => {
                 if (err) {
@@ -79,6 +81,72 @@ let theDb = {
 
         })
     },
+    createAccount: function(accId, accName) {
+        let sql = `
+        INSERT INTO accounts (user_id, acc_name, amount) VALUES(?, ?, ?)`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [accId, accName, 0], function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this.lastID);
+                }
+            })
+
+        })
+    },
+    updateUser: function(data) {
+        let sql = `
+        UPDATE users
+            SET name = ?,
+                pass = ?
+            WHERE id = ?`;
+
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [data.name, data.password, data.id], function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this.changes);
+                }
+            })
+
+        })
+    },
+    updateAccount: function(name, amount, id) {
+        let sql = `
+        UPDATE accounts
+            SET acc_name = ?,
+                amount = ?
+            WHERE id = ?`;
+
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [name, amount, id], function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this.changes);
+                }
+            })
+
+        })
+    },
+    deleteAccount: function(accId) {
+        let sql = `
+        DELETE FROM accounts
+        WHERE id = ?`;
+
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [accId], function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(this.changes);
+                }
+            })
+
+        })
+    },
     connectUserToMoney: function(userId) {
         let sql = `
         INSERT INTO money (user_id, amount) VALUES(?, ?)`;
@@ -91,6 +159,25 @@ let theDb = {
                 }
             })
 
+        })
+    },
+    getAccount: function(user) {
+        let sql = `
+        SELECT a.id, a.acc_name, a.amount
+            FROM accounts AS a
+            INNER JOIN users AS u
+                ON u.id = a.user_id
+        WHERE u.id = (SELECT id FROM users WHERE name = ?)`;
+
+        let result = [];
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [user], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
         })
     }
 }
